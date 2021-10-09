750178da80cfd102534c735ce55e0f408aec1a55
sambyalharsh06 <66405079+sambyalharsh06@users.noreply.github.com>
2021-10-09 11:23:59 +0530

    Delete script.js

--- templates/script.js
+++
@@ -1,8 +0,0 @@
-navigator.mediaDevices.getUserMedia({ audio: true })
-      .then(function(stream) {
-        console.log('You let me use your mic!')
-      })
-      .catch(function(err) {
-        console.log('No mic for you!')
-      });
-      
