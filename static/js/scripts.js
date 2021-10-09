0070e17d301948260770b7d08707cb132ad7834e
sambyalharsh06 <66405079+sambyalharsh06@users.noreply.github.com>
2021-10-09 11:26:28 +0530

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,9 @@
 // console.log("JS file included");
+navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(function(stream) {
+        console.log('You let me use your mic!')
+      })
+      .catch(function(err) {
+        console.log('No mic for you!')
+      });
+      
